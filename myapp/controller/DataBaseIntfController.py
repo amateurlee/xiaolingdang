85676fc5743604a97a9ea48fe74e3d41e392ac4b
amateurlee <amateurlee@126.com>
2018-04-09 14:10:01 +0800

    get all data from db

--- myapp/controller/DataBaseIntfController.py
+++ myapp/controller/DataBaseIntfController.py
@@ -3,11 +3,13 @@ from flask import render_template
 from .. import app
 from myapp.datacenter.db.dbtool import DBTool
 
-@app.route('/getFullData/ag')
-def getFullData():
-    user = { 'nickname': '^_^' } # fake user
+@app.route('/getFullData/cn/all')
+def getFullDataCnAll():
+    '''
+        获取所有A股数据包的数据
+    '''
     dbTool = DBTool()
     data = dbTool.getIndexTableCnData()
-    return render_template("IndexModelData.html", data = data)
+    return render_template("cnAlldata.html", data = data)
 
 
